b38de14b89b7c3cb461e479bd0e6eac7d85a9d33
AmayaJ <35116594+AmayaJ@users.noreply.github.com>
2021-06-13 23:20:49 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+
+        CascadeClassifier faceDetector = new CascadeClassifier();
+
+        faceDetector.load("C:\\opencv\\sources\\data\\lbpcascades\\lbpcascade_frontalface.xml");
+        System.out.println ( "Working" );
+        // Input image
+        Mat image = Imgcodecs.imread("C:\\Users\\amaya\\eclipse-workspace3\\DeteccionRostros\\images\\imagenGente.jpg");
+
+        // Detecting faces
+        MatOfRect faceDetections = new MatOfRect();
+        faceDetector.detectMultiScale(image, faceDetections);
+
+        // Creating a rectangular box showing faces detected
+        Rect rectCrop=null;
+        for (Rect rect : faceDetections.toArray())
+        {
+            Imgproc.rectangle(image, new Point(rect.x, rect.y),
+             new Point(rect.x + rect.width, rect.y + rect.height),
+                                           new Scalar(0, 255, 0));
+            rectCrop = new Rect(rect.x, rect.y, rect.width, rect.height);
+        }
+
+        // Saving the output image
+        String filename = "Ouput.jpg";
+        Imgcodecs.imwrite("C:\\Users\\amaya\\eclipse-workspace3\\DeteccionRostros\\images\\"+filename, image);
+
+        Mat markedImage = new Mat(image,rectCrop);
+        Imgcodecs.imwrite("C:\\Users\\amaya\\eclipse-workspace3\\DeteccionRostros\\images\\cropimage.jpg",markedImage );
+    }
+}
